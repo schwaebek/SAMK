5a5bb66bb5e12d211fc9783f26d7e49803f7eb91
Katlyn Schwaebe <schwaebek@gmail.com>
2014-09-22 10:37:38 -0400

    SAMK - basic file setup

---
+++ SAMK/Character.swift
@@ -0,0 +1,13 @@
+//
+//  Character.swift
+//  SAMK
+//
+//  Created by Katlyn Schwaebe on 9/22/14.
+//  Copyright (c) 2014 Katlyn Schwaebe. All rights reserved.
+//
+
+import UIKit
+
+class Character: NSObject {
+   
+}
---
+++ SAMK/PlayerConnect.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerConnect.swift
+//  SAMK
+//
+//  Created by Katlyn Schwaebe on 9/22/14.
+//  Copyright (c) 2014 Katlyn Schwaebe. All rights reserved.
+//
+
+import UIKit
+
+class PlayerConnect: NSObject {
+   
+}
